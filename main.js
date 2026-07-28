e3fd3ec907efec21d7214e1a06f4197f5d41b86a
antoniolorencini1977 <antonio.lorencini@escola.pr.gov.br>
2026-07-28 12:16:55 -0300

    lll

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// Seleciona o botão e a área de conteúdo do inglês
+// Seleciona o botão e a caixa de conteúdo do inglês
 const btnIngles = document.getElementById('btn-ingles');
 const conteudoIngles = document.getElementById('conteudo-ingles');
 
-// Evento para mostrar/ocultar a caixa de texto ao clicar
+// Evento de clique para abrir/fechar a aba com os 3 parágrafos
 btnIngles.addEventListener('click', function() {
     conteudoIngles.classList.toggle('ativo');
 });
