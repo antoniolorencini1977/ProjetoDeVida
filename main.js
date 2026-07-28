be5bcc0f122aebff3cc3175b0adcf852d6e069a8
antoniolorencini1977 <antonio.lorencini@escola.pr.gov.br>
2026-07-28 12:33:51 -0300

    hh

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const conteudoIngles = document.getElementById('conteudo-ingles');
 const btnEnem = document.getElementById('btn-enem');
 const conteudoEnem = document.getElementById('conteudo-enem');
 
+// Seleciona os elementos do botão e conteúdo do Vestibular
+const btnVestibular = document.getElementById('btn-vestibular');
+const conteudoVestibular = document.getElementById('conteudo-vestibular');
+
 // Evento de clique para o Inglês
 btnIngles.addEventListener('click', function() {
     conteudoIngles.classList.toggle('ativo');
@@ -14,4 +18,9 @@ btnIngles.addEventListener('click', function() {
 // Evento de clique para o ENEM
 btnEnem.addEventListener('click', function() {
     conteudoEnem.classList.toggle('ativo');
+});
+
+// Evento de clique para o Vestibular
+btnVestibular.addEventListener('click', function() {
+    conteudoVestibular.classList.toggle('ativo');
 });
